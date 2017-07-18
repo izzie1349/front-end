8cd752fd5552bf0e2429e132153189fec3dc170c
izzie1349 <izzie1349@gmail.com>
2017-07-18 15:23:52 -0400

    js: exercise using alerts, prompts, and vars

---
+++ JavaScript/js_basics/exercise_one_script.js
@@ -0,0 +1,4 @@
+var weightInPounds = prompt("Please tell me your weight (lbs):");
+var weightInKilograms = weightInPounds*(0.454);
+alert("Your weight (kgs): " + weightInKilograms);
+console.log("conversion completed");
