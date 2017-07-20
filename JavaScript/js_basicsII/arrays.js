0e161346b8a957bcf59fc89c2a2495110315b351
izzie1349 <izzie1349@gmail.com>
2017-07-20 10:29:25 -0400

    js: refactor using functions

--- JavaScript/js_basicsII/arrays.js
+++ JavaScript/js_basicsII/arrays.js
@@ -1,29 +1,38 @@
 var studentRoster = []
 var response = prompt("would you like to start the roster app? (y/n)")
 
-while (response == "y") {
-  var action = prompt('Please select an action: add, remove, display, or quit')
+function addName() {
+  var newName = prompt("what name would yo like to add?")
+  studentRoster.push(newName)
+}
 
-  if (action == "add") {
-    var nameAdd = prompt("what name would yo like to add?")
-    studentRoster.push(nameAdd)
-  }
-  else if (action == "remove") {
-    var nameRemove = prompt("what name would yo like to remove?")
-
-    for (var i=0; i < studentRoster.length-1; i++){
-      if (studentRoster[i] === nameRemove) {
-        studentRoster.splice(i,1);
-      }
+function removeName() {
+  var searchName = prompt("what name would you like to remove?")
+  for (var i=0; i<studentRoster.length-1; i++) {
+    if (studentRoster[i] === searchName) {
+      studentRoster.splice(i, 1)
     }
-  }
-  else if (action == "display") {
-    console.log(studentRoster);
-
-  }
-  else if (action == "quit") {
-    break
   }
 }
 
+function display() {
+  console.log(studentRoster);
+}
+
+while (response=="y") {
+  var action = prompt('Please select an action: add, remove, display, or quit')
+
+  if (action=="add") {
+    addName()
+  }
+  else if (action=="remove") {
+    removeName()
+  }
+  else if (action=="display") {
+    display()
+  }
+  else if (action=="quit") {
+    break
+  }
+}
 alert("Thanks fo visiting--refresh the page to start over!")
